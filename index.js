16670244758fa5e4d2a64242629543b8a70afd11
Felipe PC-Meta <felipe.fernandes@meta.com.br>
2019-06-04 09:37:43 -0300

    2.4 Saving to a Database - Working with Data and APIs in JavaScript

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
+const Datastore = require('nedb');
 
 const app = express();
-
-app.listen(3000, () => console.log('Listening at 3000'));
 
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.post('/api', (request, response) => {
-    console.log(request.body);
-    // console.log(response);
+    
+    let data = request.body;
+    let timestamp = Date.now();
+    data.timestamp = timestamp;
+    
+    
+    database.insert(data);
+    
     response.json({
         status: 'success',
-        latitude: request.body.lat,
-        longitude: request.body.long
+        timestamp: timestamp,
+        latitude: data.lat,
+        longitude: data.lon
     })
 });
+
+
+
+
+app.listen(3000, () => console.log('Listening at 3000'));
